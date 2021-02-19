2fc86fae3fb067ecd7a90443a8faf534a385e70d
Ran Tzur <rantzrur@gmail.com>
2021-02-19 20:43:57 +0200

    updated url, added package_data

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='gloat-matcher',
-    version='0.1',
+    version='0.2',
     description='Backend assigment for Gloat',
     packages=find_packages(include=['src', 'src.*']),
-    download_url='https://github.com/Rantzur1992/gloat-backend/archive/0.1.tar.gz',
+    package_data={'': ['database.json']},
+    download_url='https://github.com/Rantzur1992/gloat-backend/archive/0.2.tar.gz',
     python_requires='>3.7',
     install_requires=[
         'Click'
